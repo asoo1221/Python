e32a829ceafff681ad0a10fa36c1625a26992e6e
asoo1221 <43312921+asoo1221@users.noreply.github.com>
2018-11-21 20:07:21 +0000

    Add files via upload

---
+++ floatpoint.py
@@ -0,0 +1,18 @@
+"""""
+Asoo Azad
+"""
+
+average = 0
+sum = 0
+total = 0
+
+counter = 0
+while counter < 5:
+    counter +=1
+    total += (float(input("Enter number here")))
+    print(float_point)
+
+
+average = (total/5)
+
+print("The average is", average)
