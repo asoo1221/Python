adb158bd5cddd91b2496c296f07fab869c2e1b2b
asoo1221 <43312921+asoo1221@users.noreply.github.com>
2020-07-03 16:37:41 +0100

    Update tsubot.py

--- tsubot.py
+++ tsubot.py
@@ -3,65 +3,89 @@ Asoo Azad
 TsuBot
 Discord Bot
 """
-import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
-import asyncio
-import time
 import random
+import time
+import asyncio
+from discord.ext import commands
+from discord.ext.commands import Bot
+import discord
+import datetime
+print(discord.__version__)
+
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = "!")
+client = commands.Bot(command_prefix="!")
 
 
 @client.event
-async def on_ready() :
+async def on_ready():
     print("Bot is ready")
 
 
 @client.event
 async def on_message(message):
-        if message.content.lower() == "cookie":
-            await client.send_message(message.channel, ":cookie:")
-        elif message.content.lower() == "ok":
-            await client.send_message(message.channel, ":ok_hand:")
-        elif message.content.lower() == "love":
-            await client.send_message(message.channel, ":heart:")
-        elif message.content.lower() == "kys":
-            await client.send_message(message.channel, ":gun:")
-        elif message.content.lower() == "bye":
-            await client.send_message(message.channel, ":wave:")
+    if message.content.lower() == "cookie":
+      await message.channel.send(":cookie:")
+    elif message.content.lower() == "ok":
+            await message.channel.send(":ok_hand:")
+    elif message.content.lower() == "love":
+            await message.channel.send(":heart:")
+    elif message.content.lower() == "kys":
+            await message.channel.send(":gun:")
+    elif message.content.lower() == "bye":
+            await message.channel.send(":wave:")
 
-        # commands
+    # commands
 
-        if  message.content.startswith("$hello"):
-            await client.send_message(message.channel, "`hello, how are you?`")
-        elif message.content.startswith("$friend"):
-            await client.send_message(message.channel, "`Yes, :3`")
-        elif message.content.startswith("$love"):
-            await client.send_message(message.channel, "`i love you <3`")
-        elif message.content.startswith("$noob"):
-            await client.send_message(message.channel, "` you are noobs!`")
-        elif message.content.startswith("$hate"):
-            await client.send_message(message.channel, "i hate you :angry:")
-        elif message.content.startswith("$commands"):
-            await client.send_message(message.channel, "`Commands:`" "\n" "`$hello`" "\n" "`$friend`" "\n" "`$love`" "\n" "`$noob`" "\n" "`$hate`"  "\n" "`$magicball`" )
+    if message.content.startswith("$hello"):
+            await message.channel.send("hello, how are you?")
+            await message.channel.send('https://gph.is/1JksT3z')
+    elif message.content.startswith("$friend"):
+            await message.channel.send("Yes, :3")
+    elif message.content.startswith("$love"):
+            await message.channel.send("i love you <3")
+    elif message.content.startswith("$noob"):
+            await message.channel.send("you are noobs!")
+    elif message.content.startswith("$hate"):
+            await message.channel.send("i hate you :angry:")
+    elif message.content.startswith("$commands"):
+            await message.channel.send("`Commands:`" "\n" "`$hello`" "\n" "`$friend`" "\n" "`$love`" "\n" "`$noob`" "\n" "`$hate`"  "\n" "`$magicball`")
 
-        # warnings
-        if message.content.startswith("!spamwarn"):
-            await client.send_message(message.channel, ":ok_hand: STOP SPAMMING")
-        elif message.content.startswith("!firstwarn"):
-            await client.send_message(message.channel, ":ok_hand: First warning")
-        elif message.content.startswith("!finalwarning"):
-            await client.send_message(message.channel, ":ok_hand: Last warning! One more time and its a ban! :angry:")
+    # warnings
 
-        chatFilter = ['fuck', 'shit', 'piss', 'nigger', 'Kai' , 'Chai']
-            # messageWords = message.content.split(" ")
-        if message.content.startswith("$magicball"):
+    if message.content.startswith("!spamwarn"):
+           await message.channel.send(":ok_hand: STOP SPAMMING")
+    elif message.content.startswith("!firstwarn"):
+            await message.channel.send(":ok_hand: First warning")
+    elif message.content.startswith("!finalwarning"):
+            await message.channel.send(":ok_hand: Last warning! One more time and its a ban! :angry:")
+
+    if message.content.startswith("$magicball"):
             ball8 = random.choice(
-             ['It is certain', 'As i see it, yes', 'Dont count on it', 'Without a doubt', 'Definitely', 'Very doubtful','Outlook not so good', 'My sources say no', 'My reply is no', 'Most likely', 'You may rely on it',
+             ['It is certain', 'As i see it, yes', 'Dont count on it', 'Without a doubt', 'Definitely', 'Very doubtful', 'Outlook not so good', 'My sources say no', 'My reply is no', 'Most likely', 'You may rely on it',
              ])
-            await client.send_message(message.channel, ball8)
+            await message.channel.send(ball8)
+
+   # welcome
+
+    if message.content.startswith("welcome"):
+           await message.channel.send('Working')
+           await message.channel.send('https://media1.giphy.com/media/yyVph7ANKftIs/giphy.gif')
+
+   # Time
+
+    if message.content.startswith("!time"):
+            t = datetime.datetime.now()
+            embed=discord.Embed(title="Time", colour = random.randint(0, 0xFFFFFF))
+            embed.add_field(name = "GMT + 1 (Ireland)", value=t.strftime('%H:%M:%S %p'), inline=False)
+            embed.set_thumbnail(url = 'https://i.imgur.com/SSWPDvX.gif')
+            embed.set_footer(text="Msg for help")
+            await message.channel.send(embed=embed)
+
+    
+
+
+        
 
 
 
@@ -69,4 +93,5 @@ async def on_message(message):
 
 
 
-client.run("NDk2NDI1OTk1MzkwMjg3ODgy.DpQcXg.zMOqOJPZS94mPbOI90OSCFavbPM")
+
+client.run('')
